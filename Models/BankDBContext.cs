bbce74e36d2e3090da9c8440369285c5c00aaa7a
Mohan <n01620006@humber.ca>
2024-04-18 12:25:33 -0400

    fixed connection string

--- Models/BankDBContext.cs
+++ Models/BankDBContext.cs
@@ -59,7 +59,7 @@ public partial class BankDBContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:ironbank32.database.windows.net,1433;Initial Catalog=BankDB;Persist Security Info=False;User ID=ironbank;Password=Password32;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        => optionsBuilder.UseSqlServer("your server connection string here");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
